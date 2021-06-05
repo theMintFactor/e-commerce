f34087803ab35bf9f8f22e8fbeab7ed7fa636cea
dimple-shah-au13 <dimps287980@gmail.com>
2021-06-05 12:33:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
+
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
--- src/index.js
+++ src/index.js
@@ -5,11 +5,24 @@ import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+//store
+const store = createStore(rootReducer, composeWithDevTools());
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  //<React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+      </BrowserRouter>
+    </Provider>,
+  //</React.StrictMode>,
   document.getElementById('root')
 );
+
+
